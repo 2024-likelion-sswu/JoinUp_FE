e7641f22350e5e4619197665c6bb255570adff86
BB545 <black1016@hanmail.net>
2024-12-31 21:24:56 +0900

    Feat: 상단바 구현 상단바 컴포넌트 구현완료, 페이지 별 상단바 적용 및 title props 설정 필요

--- JoinUp_FE/fe/src/components/header_section/Header.jsx
+++ JoinUp_FE/fe/src/components/header_section/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
-    <div>
-        
+    <div id='header_container'>
+      <div id="header_inner_container">
+        <div id="header_inner_box">
+          <div className="header_inner_back">
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="18" viewBox="0 0 10 18" fill="none" className='header_back_icon'>
+              <path d="M9 1L1 9L9 17" stroke="#333333" stroke-linecap="round" />
+            </svg>
+          </div>
+          <span className="header_inner_title">{title}</span>
+          <div className="header_inner_blank"></div>
+        </div>
+      </div>
     </div>
   )
 }

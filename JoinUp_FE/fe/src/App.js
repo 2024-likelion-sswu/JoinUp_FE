c391ca2036a040ef800f874c14f89d847ed26934
BB545 <black1016@hanmail.net>
2025-01-01 23:46:59 +0900

    Fix: app.js 하단바 적용 오류 수정 app.js에 하단바 적용 경로 제외 방법이 잘못되어 다른 방법으로 수정

--- JoinUp_FE/fe/src/App.js
+++ JoinUp_FE/fe/src/App.js
@@ -1,21 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Main from "./pages/Main";
 import Footer from "./components/footer_section/Footer";
 import LoginPage from "./pages/Login/LoginPage";
 import SignupPage from "./pages/Login/SignupPage";
+import LiveChatListPage from "./pages/Taxi_chatting/LiveChatListPage";
+import LiveChatPage from "./pages/Taxi_chatting/LiveChatPage";
 
 function App() {
-  const exceptPath = ['/login', '/signup'];
+  const AppContent = () => {
+    const location = useLocation();
+    const exceptPath = ['/login', '/signup'];
+    const shouldShowFooter = !exceptPath.includes(location.pathname);
 
-  return (
-    <div className="App">
-      <BrowserRouter>
+    return (
+      <>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/livechat" element={<LiveChatListPage />} />
+          <Route path="/livechat/id" element={<LiveChatPage />} />
         </Routes>
-        {!exceptPath && <Footer />}
+        {shouldShowFooter && <Footer />}
+      </>
+    );
+  };
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppContent />
       </BrowserRouter>
     </div>
   );
